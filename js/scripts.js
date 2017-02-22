cc1f0a8e62cabcb330cfecfd315ce3b477014dca
Clifford Grimmell and jahan walsh <clifford@>
2017-02-22 10:14:38 -0800

    add output of success should funtion run correctly

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,20 @@ $(function(){
   $("form#pig-form").submit(function(event){
     event.preventDefault();
     var input = $("input#input").val();
-      console.log(input); 
+    var split = input.split(" ");
+    split.forEach(function(x){
+      var x = x.split("");
+      x.forEach(function(y){
+        if(isNaN(y)=== true){
+          if (y === "a") {
+          console.log("success");
+          } else {
+          console.log("failure");
+        } $("#output").show();
+        } else {
+          alert("Please enter a valid sentence");
+        }
+      });
+    });
   });
 });
