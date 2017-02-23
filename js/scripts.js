932ae8ab9636aa7c2463d16a1baf97ec08fabee1
Clifford Grimmell and jahan walsh <clifford@>
2017-02-22 18:59:15 -0800

    add functionality to script allowing it to convert a lower case sentence to pig latin. further updates would allow the string qu to be moved together and numbers not to be converted

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,37 @@
+// This is all backend
+
 var letters = [];
 var words = [];
 var vowel = ["a","e","i","o","u","y"];
 var consonant = ["b","c","d","f","g","h","j","k","l","m","n","p","q","r","s","t","v","w","x","z"];
-var alphabet = [vowel, consonant];
+var sentence = [];
 var selector = function(letter) {
-  alphabet.forEach(function(alph) {
-    if(isNaN(letter)=== true) {
-      for (var x = 0; x <= alph.length ; x++ ) {
-        //debugger;
+      for (var x = 0; x <= vowel.length; x++ ) {
         if (letter[0] === vowel[x]) {
           letter.push("ay");
-          console.log(letter);
-        } else if (letter[0] === consonant[x]) {
-          var thisLetter = letter[0];
-          letter.push(thisLetter + "ay");
-          console.log(letter);
+          var joined1 = letter.join("");
+          console.log(joined1);
+          sentence.push(joined1);
+
+        } else if ((letter[0] !== vowel[0]) && (letter[0] !== vowel[1]) && (letter[0] !== vowel[2]) && (letter[0] !== vowel[3]) && (letter[0] !== vowel[4]) && (letter[0] !== vowel[5])) {
+          return null;
       }
-  }} else {
-      alert("Please enter a valid sentence");
+    }
+  };
+
+var selector2 = function(letter) {
+  for (var y = 0; y <= consonant.length; y++) {
+   if (letter[0] === consonant[y]) {
+    var thisLetter = letter[0];
+    letter.push(thisLetter);
+    letter.splice(0,1);
+    return letter;
+    }
   }
-});
 };
 
-$(function(){
-  $("form#pig-form").submit(function(event){
+$(function(){ //document.ready function
+  $("form#pig-form").submit(function(event){ //main submit function
     event.preventDefault();
     var input = $("input#input").val();
     var split = input.split(" ");
@@ -31,9 +39,28 @@ $(function(){
       var letters = words.split("");
       console.log(letters);
       for (var i=0; i <= (letters.length-letters.length); i++) {
-        selector(letters);
-      };
-        });
+        if (selector(letters) === null){
+          if (selector2(letters)=== letters) {
+            selector2(letters);
+            letters.push("ay")
+            var joined2 = letters.join("");
+            console.log(joined2);
+            sentence.push(joined2);
+          } else {
+            letters.push("ay")
+            var joined3 = letters.join("");
+            console.log(joined3);
+            sentence.push(joined3);
+          }
 
-      });
+        };
+      };
     });
+
+    //This is all front end
+    var wordsJoin = sentence.join(" ");
+    console.log(wordsJoin);
+    $("#output").text(wordsJoin);
+
+  });
+});
